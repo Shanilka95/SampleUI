07b4c202a751316d770a3272254a5841c70b97f6
shanilka <shanilka@zdsdevops.com>
2022-01-20 18:49:00 +0530

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import { name as appName } from './app.json';
+import Sample from './src/Sample';
+
+AppRegistry.registerComponent(appName, () => Sample);
